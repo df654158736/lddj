38f6c4cce57570c1fef7b32503eea2b0f3f9869a
df654158736 <df654158736@gmail.com>
2024-08-06 16:12:19 +0800

    add:grpc verify_code

--- backend/verifyCode/internal/server/grpc.go
+++ backend/verifyCode/internal/server/grpc.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	v1 "verifyCode/api/helloworld/v1"
+	s "verifyCode/api/s/v1"
 	"verifyCode/internal/conf"
 	"verifyCode/internal/service"
 
@@ -11,7 +12,7 @@ import (
 )
 
 // NewGRPCServer new a gRPC server.
-func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *grpc.Server {
+func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, verify_code *service.VerifyCodeService, logger log.Logger) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
@@ -28,5 +29,6 @@ func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 	}
 	srv := grpc.NewServer(opts...)
 	v1.RegisterGreeterServer(srv, greeter)
+	s.RegisterVerifyCodeServer(srv, verify_code)
 	return srv
 }
